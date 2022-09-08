7c2398e25281d17c47f61b9d3dc7b98368d30182
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2022-09-08 10:00:48 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,26 @@
-function start() {
+const userMedia = await navigator.mediaDevices.getUserMedia({audio : true, video : true});
+const videoElement = document.getElementById("localVideo");
 
+videoElement.srcObject = userMedia;
+
+const videoTrack = userMedia.getVideoTracks()[0];
+const audioTrack = userMedia.getAudioTracks()[0];
+const screenMedia = await navigator.mediaDevices.getDisplayMedia();
+
+const stream = new MediaStream();
+stream.addTrack(screenMedia);
+stream.addTrack(videoTrack);
+stream.addTrack(audioTrack);
+
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+
+startBtn.onclick = async () => {
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+videoElement.play();
 }
-
-function stop() {
-
+stopBtn.onclick = async () => {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
 }
