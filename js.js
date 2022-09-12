0aa4092effdfd159fe85a621a3906acf21a4f93f
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2022-09-12 12:52:14 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,25 +1,23 @@
+async function asyncCall() {
 const startBtn = document.getElementById('startbtn');
 const stopBtn = document.getElementById('stopbtn');
  
-const userMedia = navigator.mediaDevices.getUserMedia({audio : true, video : true});
+const userMedia = await navigator.mediaDevices.getUserMedia({audio : true, video : true});
 
 const videoElement2 = document.getElementById("localVideo1");
 const videoElement1 = document.getElementById("localVideo2");
 
 const videoTrack = userMedia.getVideoTracks()[0];
 const audioTrack = userMedia.getAudioTracks()[0];
-const screenMedia = navigator.mediaDevices.getDisplayMedia();
+const screenMedia = await navigator.mediaDevices.getDisplayMedia();
 
  videoTrack.enabled = false;
   audioTrack.enabled = false;
-
-const stream = new MediaStream();
-stream.addTrack(videoTrack);
-stream.addTrack(audioTrack);
   
 videoElement2.srcObject = userMedia;
 videoElement1.srcObject = screenMedia;
-  
+}
+asyncCall();
 startBtn.addEventListener('click', () => {
 videoTrack.enabled = true;
 audioTrack.enabled = true;
